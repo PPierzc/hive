9cc3fd7a24434654bb322cd5e94c19d66841a4ff
paul <ppierzc@gmail.copm>
2023-05-23 17:19:16 +0200

    fix: clean up and improve styling

--- hive/cli.py
+++ hive/cli.py
@@ -14,47 +14,11 @@ client = QdrantClient(path="./db")
 app = typer.Typer()
 console = Console()
 
-# data_dir = "./data"
-#
-# # get all the .md files in the data directory
-# files = [
-#     os.path.join(data_dir, file)
-#     for file in os.listdir(data_dir)
-#     if file.endswith(".md")
-# ]
-
 
 @app.command()
 def search(query: str):
     # Loader with the text "Searching for 'query'..."
     with BeeStatus(f"Parsing files") as status:
-        # read the files and add them to the data list
-        # data = []
-        # for file in files:
-        #     with open(file, "r") as f:
-        #         # read the text
-        #         body = f.read()
-        #         # split the text into paragraphs
-        #         paragraphs = body.split("\n\n")
-        #         # skip paragraphs that are too short (less than 50 characters) or are just a link, image, or code block
-        #         paragraphs = [
-        #             paragraph
-        #             for paragraph in paragraphs
-        #             if len(paragraph) > 250
-        #                and not paragraph.startswith("!")
-        #                and not paragraph.startswith("http")
-        #                and not paragraph.startswith("    ")
-        #                and not paragraph.startswith("#")
-        #                and not paragraph.startswith("$")
-        #                and not paragraph.startswith("<")
-        #                and not paragraph.startswith("[")
-        #                and not paragraph.startswith("---")
-        #         ]
-        #
-        #         # add the paragraphs to the data list
-        #         for paragraph in paragraphs:
-        #             data.append({"filename": file, "body": paragraph})
-
         status.update(f"Searching paragraphs for '[bold]{query}[/bold]'...")
 
         from sentence_transformers import SentenceTransformer
@@ -79,7 +43,7 @@ def search(query: str):
         console.print(f"No results found for '{query}'")
         return
 
-    header = f"[bold]Search results for '{query}'[/bold]."
+    header = f"[bold]Search results for \"{query}\"[/bold]."
     console.print(header)
     console.print()
 
@@ -97,6 +61,7 @@ def search(query: str):
             expand=False,
             width=120,
             padding=(1, 2),
+            border_style="#ffa908"
         )
         console.print(panel)
         # add a line break
@@ -115,7 +80,7 @@ def add(path: str):
     model = SentenceTransformer("msmarco-distilbert-base-dot-prod-v3")
 
     if os.path.isfile(path):
-        console.print(f"Adding file: {path}")
+        console.print(f"Adding file: {path}", style='#ffa908')
         with open(path, "r") as f:
             body = f.read()
             paragraphs = body.split("\n\n")
@@ -162,10 +127,32 @@ def add(path: str):
 
 @app.command()
 def init():
-    client.recreate_collection(
-        collection_name="paragraphs",
-        vectors_config=rest.VectorParams(size=768, distance=rest.Distance.COSINE),
-    )
+    welcome = """   
+ |_|  o       _  
+ | |  |  \/  (/_ 
+ """
+
+    console.print(welcome, style="#ffa908")
+
+    with BeeStatus(f"Initializing model") as status:
+        from sentence_transformers import SentenceTransformer
+
+        SentenceTransformer("msmarco-distilbert-base-dot-prod-v3")
+
+        status.update(f"Initializing database")
+        """
+        Create a new collection with a vector index
+        """
+        client.recreate_collection(
+            collection_name="paragraphs",
+            vectors_config=rest.VectorParams(size=768, distance=rest.Distance.COSINE),
+        )
+
+    success_message = """Hive initialized successfully! 🐝
+You can now add files to the database using the [yellow]'hive add'[/yellow] command.
+"""
+
+    console.print(success_message, style="#ffa908")
 
 
 @app.command()
